9369fea1f28b929170c7174e9e683b0113e3347c
youxinweizhi <hanxiaomeng@163.com>
2019-03-28 16:37:09 +0800

    修改界面

--- AutoFlash.py
+++ AutoFlash.py
@@ -19,7 +19,7 @@ def run(com, esp_type, firmware):
             '--port', com,
             '--baud', '460800',
             'write_flash', "--flash_size=detect", '0',
-            'esp8266-20180126-v1.9.3-240-ga275cb0f.bin'
+            firmware
         ]
     else:
         sys.argv = [
